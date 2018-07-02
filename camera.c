d77bf969d979dc225b76ab93721aa816705b0ea3
djzapo <zapo93@hotmail.it>
2018-07-02 11:48:43 +0200

    libray camera

---
+++ camera.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main()
+{
+   Caputure();
+
+}
+
+//crating library for control camera from c or c++
+void Capture()// this function capture the image and save it in img.jpg
+{
+    char command[50];
+    strcpy(command,"raspistill -o test.jpg ");
+    system(command);
+    return 0;
+}
