3765cea841b586711b008e05259cf6dc5be76216
djzapo <zapo93@hotmail.it>
2018-07-02 16:38:42 +0200

    sistemato server,collegata la libbreria

--- server.c
+++ server.c
@@ -10,7 +10,6 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/sendfile.h>
-
 #include "camera.c"
 
 char webpage[]=
@@ -18,7 +17,7 @@ char webpage[]=
 "Content-Type: text/html; charset=UTF-8\r\n\r\n"
 "<!DOCTYPE html>\r\n"
 "<html lang='it'><head><title>Raspberry Pi Surveillance</title><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css' integrity='sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ' crossorigin='anonymous'><link rel='stylesheet' href='https://v4-alpha.getbootstrap.com/examples/sticky-footer/sticky-footer.css'><script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script><script src='https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js' integrity='sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb' crossorigin='anonymous'></script><script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js' integrity='sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn' crossorigin='anonymous'></script></head>\r\n"
-"<body><div class='container-fluid' style='padding-left:5%; padding-right:5%;'><!-- TITLE --><div class='row' style='padding-top: 2%'><div class='col-12 col-md-12 col-sm-12 col-xl-12'><center><h1>RASPBERRY PI VIDEO SURVEILLANCE</h1></center></div></div><!-- SEZIONE LIVE --><div class='row' style='height:60vh;'><div class='col-12 col-md-12 col-sm-12 col-xl-12'><center><img id='iframeid' src='stream/test.jpg' style='width:100%; height:100%;'></center></div></div><!-- SEZIONE COMANDI --><div class='row' style='padding-top: 1%;'><div class='col-12 col-sm-12 col-md-12' style='position:sticky;'><center><form name='control-form' method='post'><div class='form-group'><input id='comandosx' type='submit' class='btn btn-inline' style='margin: 1%;' name='comandi' value='SX (45°)'><input id='comandodx' type='submit' class='btn btn-inline' name='comandi' value='DX (45°)'></div><div class='form-group'><label><h3>Position: </h3></label><input id='coordinate' style='margin-left:1%;margin-right:1%;' placeholder='0-359°' type='number' name='position' class='form-control-inline' min='0' max='359'><input id='absoluteposition' type='submit'  name='sendposition' value='INVIA' class='btn btn-inline'></div><div class='form-group'><label><h3>Resolution: </h3></label><input id='resolutionw' placeholder='w' style=margin-left:1%;' type='number' name='position' class='form-control-inline' min='240' max='2560'><input id='resolutionh' placeholder='h' style='margin-left:1%;margin-right:1%;' type='number' name='position' class='form-control-inline' min='240' max='1920'><input id='scatta' type='submit' class='btn btn-inline btn-danger' name='comandi' value='SCATTA'></div></form></center></div></div></div><footer class='footer' style='background-color: lightgrey; padding-bottom: 1%;position:absolute; bottom: 0; width: 100%;'><div class='container' style='background-color: lightgrey'><center><span class='text-danger'>Coded by: Matteo Attenni & Raoul Nuccetelli & Mihai Zapodeanu</span></center></div></footer></body>"
+"<body><div class='container-fluid' style='padding-left:5%; padding-right:5%;height:100%'><!-- TITLE --><div class='row' style='padding-top: 2%'><div class='col-12 col-md-12 col-sm-12 col-xl-12'><center><h1>RASPBERRY PI VIDEO SURVEILLANCE</h1></center></div></div><!-- SEZIONE LIVE --><div class='row' style='height:30%;'><div class='col-12 col-md-12 col-sm-12 col-xl-12'><center><img id='iframeid' class='img-responsive' src='test.jpg' style='width:70%;height:50%;'></center></div></div><!-- SEZIONE COMANDI --><div class='row' style='padding-top: 1%;'><div class='col-12 col-sm-12 col-md-12' style='position:sticky;'><center><form name='control-form' method='post'><div class='form-group'><input id='comandosx' type='submit' class='btn btn-inline' style='margin: 1%;' name='comandi' value='SX (45°)'><input id='comandodx' type='submit' class='btn btn-inline' name='comandi' value='DX (45°)'></div><div class='form-group'><label><h3>Position: </h3></label><input id='coordinate' style='margin-left:1%;margin-right:1%;' placeholder='0-359°' type='number' name='position' class='form-control-inline' min='0' max='359'><input id='absoluteposition' type='submit'  name='sendposition' value='INVIA' class='btn btn-inline'></div><div class='form-group'><label><h3>Resolution: </h3></label><input id='resolutionw' placeholder='w' style=margin-left:1%;' type='number' name='position' class='form-control-inline' min='240' max='2560'><input id='resolutionh' placeholder='h' style='margin-left:1%;margin-right:1%;' type='number' name='position' class='form-control-inline' min='240' max='1920'><input id='scatta' type='submit' class='btn btn-inline btn-danger' name='comandi' value='SCATTA'></div></form></center></div></div></div><footer class='footer' style='background-color: lightgrey; padding-bottom: 1%;position:absolute; bottom: 0; width: 100%;'><div class='container' style='background-color: lightgrey'><center><span class='text-danger'>Coded by: Matteo Attenni & Raoul Nuccetelli & Mihai Zapodeanu</span></center></div></footer></body>"
 "</html>";
 
 char imageheader[] = "HTTP/1.1 200 Ok\r\n""Content-Type: image/jpg\r\n\r\n";
@@ -59,6 +58,7 @@ int main(){
             perror("Connessione fallita...\n");
             continue;
         }
+	Capture();
         if(!fork()){
             close(fd_server);
             memset(buf,0,2048);
@@ -67,10 +67,8 @@ int main(){
             if(!strncmp(buf,"GET /test.jpg",13)){
                 printf("Invio immagine in corso\n");
                 write(fd_client,imageheader,sizeof(imageheader)-1);
-                Capture();
-                
                 fdimg=open("test.jpg",O_RDONLY);
-                int sent=sendfile(fd_client,fdimg,NULL,10000);
+                int sent=sendfile(fd_client,fdimg,NULL,100000);
                 printf("Inviata %d:",sent);
                 close(fdimg);
             }
