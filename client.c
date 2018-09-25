03c297ebd6e688232fc7ab36d05843d0eb3ed149
Raoul <raouldonaldn@yahoo.it>
2018-09-25 13:08:42 +0200

    Nuovo client pronto per il server di Matteo.

--- client.c
+++ client.c
@@ -1,56 +1,127 @@
-#include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <netdb.h>
 #include <arpa/inet.h>
+#include <pthread.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
-#include <time.h>
-#include <pthread.h>
+#include <signal.h>
 
+pthread_t receiving_thread;
+char server_reply[20000];
+char message[1024];
 
-void ChiudiSocket(int sock)
-{
-  int ret=close(sock)
-  if(ret<0)
-     perror ("Errore chiusura socket");
-  return;
+typedef struct receiving_args_s{
+  int descr;
+} receiving_args_t;
+
+int sock=0;
+
+void kill_handler(int sign) {
+  puts("Ricevuto segnale di chiusura, chiudo tutto!");
+  if(sock != 0) close(sock);
+  exit(1);
 }
-void CreaSocket(char* Destinazione,int Porta)
-{
-    struct sockaddr_in temp;
-    struct hostent *h;
-    int sock;
-    int errore;
-    //Tipo di indirizzo
-    temp.sin_family=AF_INET;
-    temp.sin_port(htons(Porta));
-    h=gethostbyname(Destinazione);
-    if(h==0)
-    {
-        printf("Gethostbyname fallito\n");
-        exit(1);
+
+void* recevingPhoto(void* arg) {
+    struct sockaddr_in server;
+
+    int photo_sock = socket(AF_INET , SOCK_STREAM , 0);
+    if (photo_sock == -1){
+        printf("errore creazione socket");
     }
-    bcopy(h->h_addr,&temp.sin_addr,h->h_length);
-    //creazione socket
-    sock=socket(AF_INET,SOCK_STREAM,0);
-    //connessione del socket in caso di errore gestiamo con l'oportuno errore
-   errore=connect(sock,(struct sockaddr*) &temp,sizeof(temp));
-   if(errore<0)
-     {perror("Errore connessione socket!! riprova");
-       exit(1);
-     }
-   return sock;
+    puts("Socket creato");
+
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons( 3000 );
+
+    if (connect(photo_sock , (struct sockaddr *)&server , sizeof(server)) < 0){
+        perror("Non trovo un server a cui collegarmi!");
+        pthread_exit(NULL);
+    }
+    puts("Connesso correttamente");
+
+    int ret;
+    printf("%ld",sizeof(server_reply));
+    while(1){
+        ret = recv(photo_sock , server_reply , sizeof(server_reply) , 0);
+        if(ret <= 0){
+            puts("errore collegamento");
+            close(photo_sock);
+            exit(1);
+        }
+        int server_reply_size = ret;
+        printf("Dimensione del file: %d\n",server_reply_size);
+        if(server_reply_size > 5000){
+            // scrittura su file
+            FILE* foto;
+            foto = fopen("test2.jpg","wb");
+            fwrite(server_reply,1,server_reply_size,foto);
+            fclose(foto);
+            puts("ho memorizzato il file");
+        }
+    }
 }
 
-int main(int argc,char* argv)
-{
-  int socket_server;
+
+int main(int argc , char *argv[]){
+    struct sockaddr_in server;
+
+    sock = socket(AF_INET , SOCK_STREAM , 0);
+    if (sock == -1){
+        printf("errore creazione socket");
+    }
+    puts("Socket creato");
+
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons( 3000 );
+
+    signal(SIGINT,kill_handler);
+
+    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0){
+        perror("non trovo server a cui collegarmi");
+        return 1;
+    }
+
+    puts("Connesso correttamente");
 
 
+    receiving_args_t* receive_thread_args=malloc(sizeof(receiving_args_t));
+    receive_thread_args->descr=sock;
 
+    int ret=pthread_create(&receiving_thread,NULL,recevingPhoto,(void*)receive_thread_args);
+    if(ret!=0){
+        perror("Errore durante la creazione del thread di cattura della foto");
+        return 1;
+    }
 
+    // interazione a domande col server per la modifica di parametri di risoluzione/movimento del servo
+    while(1){
+        while ((ret = recv(sock, message, 1024, 0)) < 0 ) {
+            if (errno == EINTR) continue;
+            if(ret == -1) {
+                fprintf(stderr,"Errno on recv: %d, Error message: %s\n",errno,strerror(errno));
+                return -1;
+            }
+        }
+        message[ret] = '\0';
+
+        puts("il server dice :");
+        puts(message);
+
+        printf("inserire comando da inviare : ");
+        scanf("%s" , message);
+
+        //message contiene il messaggio che manda il client!
+        if( send(sock , message , strlen(message) , 0) < 0){
+            puts("invio fallito");
+            return 1;
+        }
+    }
+
+    close(sock);
+    return 0;
 }
